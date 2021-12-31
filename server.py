f88b9a8296d463ce463f4108e970daacf704964d
ozzzzz <you@example.com>
2021-12-31 17:21:43 +0300

    oop start

--- server.py
+++ server.py
@@ -3,60 +3,61 @@ import socket
 import sys
 
 from common.utils import get_message, send_message
-from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, RESPONDEFAULT_IP_ADDRESSSE, ERROR, \
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, RESPONDEFAULT_IP_ADDRESSES, ERROR, \
     DEFAULT_PORT, MAX_CONNECTIONS
 
 
-def process_client_message(message):
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and USER in message \
-            and message[USER][ACCOUNT_NAME] == 'Guest':
-        return {RESPONSE: 200}
-    return {
-        RESPONDEFAULT_IP_ADDRESSSE: 400,
-        ERROR: 'Bad Request'
-    }
+class Server:
+    def process_client_message(self, message):
+        if ACTION in message and message[ACTION] == PRESENCE and TIME in message and USER in message \
+                and message[USER][ACCOUNT_NAME] == 'Guest':
+            return {RESPONSE: 200}
+        return {
+            RESPONDEFAULT_IP_ADDRESSES: 400,
+            ERROR: 'Bad Request'
+        }
 
-
-def main():
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = DEFAULT_PORT
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта')
-        sys.exit(1)
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535')
-        sys.exit(1)
-
-    try:
-        if '-a' in sys.argv:
-            listen_address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            listen_address = ''
-    except IndexError:
-        print('Послу параметра -\'a\' необходимо указать адрес, который будет слушать ')
-        sys.exit(1)
-
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.bind((listen_address, listen_port))
-    transport.listen(MAX_CONNECTIONS)
-
-    while True:
-        client, client_address = transport.accept()
+    def main(self):
         try:
-            message_from_client = get_message(client)
-            print(message_from_client)
-            response = process_client_message(message_from_client)
-            send_message(client, response)
-            client.close()
-        except (ValueError, json.JSONDecodeError):
-            print('Принято некорректное сообщене от клиента')
-            client.close()
+            if '-p' in sys.argv:
+                listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+            else:
+                listen_port = DEFAULT_PORT
+            if listen_port < 1024 or listen_port > 65535:
+                raise ValueError
+        except IndexError:
+            print('После параметра -\'p\' необходимо указать номер порта')
+            sys.exit(1)
+        except ValueError:
+            print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535')
+            sys.exit(1)
+
+        try:
+            if '-a' in sys.argv:
+                listen_address = sys.argv[sys.argv.index('-a') + 1]
+            else:
+                listen_address = ''
+        except IndexError:
+            print('Послу параметра -\'a\' необходимо указать адрес, который будет слушать ')
+            sys.exit(1)
+
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.bind((listen_address, listen_port))
+        transport.listen(MAX_CONNECTIONS)
+
+        while True:
+            client, client_address = transport.accept()
+            try:
+                message_from_client = get_message(client)
+                print(message_from_client)
+                response = self.process_client_message(message_from_client)
+                send_message(client, response)
+                client.close()
+            except (ValueError, json.JSONDecodeError):
+                print('Принято некорректное сообщене от клиента')
+                client.close()
 
 
 if __name__ == '__main__':
-    main()
+    server_start = Server()
+    server_start.main()
